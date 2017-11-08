e5cd45dc57e37deaa459acf44948e17dd7acb631
lriosmx5 <blackwarrior_l@hotmail.com>
2017-11-07 20:09:12 -0600

    Add screens SingUp issues 38

--- android/app/src/main/java/mx/devf/roominn/Fragments/ServicesFragment.kt
+++ android/app/src/main/java/mx/devf/roominn/Fragments/ServicesFragment.kt
@@ -1,19 +1,16 @@
-package mx.devf.roominn
+package mx.devf.roominn.Fragments
 
 import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.DefaultItemAnimator
-import android.support.v7.widget.LinearLayoutCompat
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
 import mx.devf.roominn.Adapters.ServicesAdapter
+import mx.devf.roominn.R
 
 /**
  * Created by Luis Rios on 07/11/2017.
