16a502c1b259a5c74362fd44f7df532c2a6b60c2
Carlos <starttheshooting@hotmail.com>
2017-11-08 13:51:22 -0600

    side-bar and house details components

--- web/room-inn/src/app/app.module.ts
+++ web/room-inn/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { appRoutes } from './routes';
 import { AppComponent } from './app.component';
 import { LogInComponent } from './log-in/log-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { HouseDetailsComponent } from './house-details/house-details.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LogInComponent,
-    SignUpComponent
+    SignUpComponent,
+    HouseDetailsComponent,
+    SideBarComponent
   ],
   imports: [
     BrowserModule,
