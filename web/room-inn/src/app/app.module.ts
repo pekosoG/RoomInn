88a1ba51f8497d977ab2633313edf15a3e025c8c
Carlos <starttheshooting@hotmail.com>
2017-11-07 21:56:50 -0600

    Fixed error in log-in component name (was sign-up)

--- web/room-inn/src/app/app.module.ts
+++ web/room-inn/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { LogInComponent } from './log-in/log-in.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignUpComponent
+    LogInComponent
   ],
   imports: [
     BrowserModule
---
+++ web/room-inn/src/app/routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router'
+// import { SignUp }
+
+
+// export const appRoutes:Routes = [
+
+//   { path: 'events', component: EventListComponent},
+//   { path: '', redirectTo: '/events', pathMatch: 'full' },
+
+// ]
