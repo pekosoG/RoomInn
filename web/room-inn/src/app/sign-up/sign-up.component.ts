f2f96250419fe7d96e106296f4a780f6924236f6
Carlos <starttheshooting@hotmail.com>
2017-11-08 09:51:19 -0600

    sign-up component angular/cli ViewEncapsulation bug fixed

--- web/room-inn/src/app/sign-up/sign-up.component.ts
+++ web/room-inn/src/app/sign-up/sign-up.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
 
